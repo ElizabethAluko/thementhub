090eea2963c11255b31b8fb402fbb468f501c5d7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-10-19 13:42:44 +0100

    Add task

--- creds.py
+++
@@ -1,8 +0,0 @@
-#database credentials
-username = "0r1l8of71umnm32es8ar"
-passwd = "pscale_pw_TaMYPkleFX9hzTZUWEIs763du7ZgGiCuitGHE19iqu0"
-db_host = "aws.connect.psdb.cloud"
-database = "connecttocourse"
-app_key = "hello123"
-api_key = "1896963109msh2cb2a05e74add57p10f279jsnd6c70a71e9a0"
-api_host = "udemy-course-scrapper-api.p.rapidapi.com"
